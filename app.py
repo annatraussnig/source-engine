d744df01ee05acc180117bacd9c390fc2464a735
pifragile <pigu@bluewin.ch>
2021-05-07 15:10:46 +0200

    load tweet from url

--- app.py
+++ app.py
@@ -1,19 +1,21 @@
+import json
 import os
 
+import twitter
 from dotenv import load_dotenv
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
-import twitter
 
 load_dotenv()
 
 api = twitter.Api(access_token_key=os.environ.get('TWITTER_ACCESS_TOKEN_KEY'),
                   access_token_secret=os.environ.get('TWITTER_ACCESS_TOKEN_SECRET'),
                   consumer_key=os.environ.get('TWITTER_CONSUMER_KEY'),
-                  consumer_secret=os.environ.get('TWITTER_CONSUMER_SECRET')
+                  consumer_secret=os.environ.get('TWITTER_CONSUMER_SECRET'),
+                  tweet_mode='extended'
                   )
 
 app = Flask(__name__)
@@ -36,8 +38,15 @@ def index():
     message = ""
     if form.validate_on_submit():
         url = form.url.data
+        find_original_tweet(url)
         message = "Looking for tweet source"
     return render_template('tweet.html', form=form, message=message)
+
+
+def find_original_tweet(url):
+    status_id = url.split('status/')[1].split('?')[0]
+    status = json.loads(api.GetStatus(status_id).AsJsonString())
+    print(status)
 
 
 # keep this as is
