96a90dae498a8adb4ef0e56f8d4b19334d50e6b5
pifragile <pigu@bluewin.ch>
2021-05-07 13:50:04 +0200

    add .env

--- app.py
+++ app.py
@@ -1,16 +1,25 @@
-from flask import Flask, render_template, redirect, url_for
+import os
+
+from dotenv import load_dotenv
+from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 
+load_dotenv()
+
 app = Flask(__name__)
 
 # Flask-WTF requires an encryption key - the string can be anything
-app.config['SECRET_KEY'] = 'C2HWGVoMGfNTBsrYQg8EcMrdTimkZfAb'
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+
+# load twitter api key from .env
+TWITTER_API_KEY = os.environ.get('TWITTER_API_KEY')
 
 # Flask-Bootstrap requires this line
 Bootstrap(app)
+
 
 class TweetForm(FlaskForm):
     url = StringField('Tweet Url:', validators=[DataRequired()])
@@ -25,6 +34,7 @@ def index():
         url = form.url.data
         message = "Looking for tweet source"
     return render_template('tweet.html', form=form, message=message)
+
 
 # keep this as is
 if __name__ == '__main__':
