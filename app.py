b770cd650d747890fad232f320bd1d725823dfb9
pifragile <pigu@bluewin.ch>
2021-05-07 14:34:47 +0200

    add broken twitter api

--- app.py
+++ app.py
@@ -6,16 +6,17 @@ from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
+import twitter
 
 load_dotenv()
+
+api = twitter.Api(access_token_key=os.environ.get('TWITTER_ACCESS_TOKEN_KEY'),
+                  access_token_secret=os.environ.get('TWITTER_ACCESS_TOKEN_SECRET'))
 
 app = Flask(__name__)
 
 # Flask-WTF requires an encryption key - the string can be anything
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
-
-# load twitter api key from .env
-TWITTER_API_KEY = os.environ.get('TWITTER_API_KEY')
 
 # Flask-Bootstrap requires this line
 Bootstrap(app)
@@ -28,6 +29,9 @@ class TweetForm(FlaskForm):
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    results = api.GetSearch(
+        raw_query="q=twitter%20&result_type=recent&since=2021-04-06&count=10")
+    print(results)
     form = TweetForm()
     message = ""
     if form.validate_on_submit():
